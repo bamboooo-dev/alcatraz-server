a6259f1269197a6545ce611773c3e396f5e7b55b
Yuya Sumie <monamonacan0926@gmail.com>
2020-11-25 01:28:05 +0900

    cities#create を作成

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -2,9 +2,23 @@ module Api
   module V1
     class CitiesController < ApplicationController
       def index
-        users = City.all
-        render json: users
+        cities = City.all
+        render json: cities, status: :ok
       end
+
+      def create
+        city = City.new(city_params)
+        if city.save
+          render json: city, status: :ok
+        else
+          render status: :bad_request
+        end
+      end
+
+      private
+        def city_params
+          params.require(:city).permit(:name, :description)
+        end
     end
   end
 end
